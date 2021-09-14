c5a44ee0288d48a69f94a3097e68c7de7c00e7ad
nhpolito <40323015+nhpolito@users.noreply.github.com>
2021-09-14 14:18:01 -0700

    split the string by X

--- September/infected.js
+++ September/infected.js
@@ -9,3 +9,16 @@
 // '1' : infected
 
 // 'X' : ocean
+
+// start: map1 = "01000000X000X011X0X"
+// end:   map1 = "11111111X000X111X0X"
+// total = 15
+// infected = 11
+// percentage = 100*11/15 = 73.33333333333333
+
+function infected(s) {
+    console.log(s.split("X"))
+    return 0;
+  }
+
+infected('01000000X000X011X0X')
