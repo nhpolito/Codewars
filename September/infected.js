05261cc847cb85a5bb45b59a572bee715d0bea19
nhpolito <40323015+nhpolito@users.noreply.github.com>
2021-09-14 14:15:53 -0700

    Started new infected js project

---
+++ September/infected.js
@@ -0,0 +1,11 @@
+// ⚠️ The world is in quarantine! There is a new pandemia that struggles mankind. Each continent is isolated from each other but infected people have spread before the warning. ⚠️
+
+// 🗺️ You would be given a map of the world in a type of string:
+
+// string s = "01000000X000X011X0X"
+
+// '0' : uninfected
+
+// '1' : infected
+
+// 'X' : ocean
