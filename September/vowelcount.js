bf12592dbbf84d3930b3dfa971d4dcc8467c938f
nhpolito <40323015+nhpolito@users.noreply.github.com>
2021-09-14 21:49:04 -0700

    What I believe is to be completed code

--- September/vowelcount.js
+++ September/vowelcount.js
@@ -1,8 +1,11 @@
 function getCount(str) {
     var vowelsCount = 0;
-    
+    let vowels = ['a','e','i','o','u']
+    let split = str.split("").filter(x => {return vowels.indexOf(x) !== -1}).length
+    console.log(split)
     // enter your majic here
     
-    return vowelsCount;
+    return split;
   }
 
+console.log(getCount('aabbccddee'))
